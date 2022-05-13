66874908940aa42a20d3db3a9a981eff927076b2
jrodr117 <jrodr113@msudenver.edu>
2022-05-12 18:50:28 -0600

    added user model and worked on stubbing up token based auth

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,12 +1,21 @@
+const User = require('../../models/users');
+const jwt = require('jsonwebtoken');
+
 module.exports = {
 	create,
 };
 
-function create(req, res) {
-	res.json({
-		user: {
-			name: req.body.name,
-			email: req.body.email,
-		},
-	});
+async function create(req, res) {
+	try {
+		// Add the user to the database
+		const user = await User.create(req.body);
+		// token will be a string
+		// Yes, we can use res.json to send back just a string
+		// The client code needs to take this into consideration
+		res.json(token);
+	} catch (err) {
+		// Client will check for non-2xx status code
+		// 400 = Bad Request
+		res.status(400).json(err);
+	}
 }
